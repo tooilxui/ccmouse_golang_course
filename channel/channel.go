fcaef57ed4d1cc9e5dd11c70e4b20becd431f0f0
Saxon C <tooilxui@gmail.com>
2020-02-23 16:49:50 +0800

    channel: chan as return value

--- channel/channel.go
+++ channel/channel.go
@@ -5,10 +5,14 @@ import (
 	"time"
 )
 
-func worker(id int, c chan int) {
-	for {
-		fmt.Printf("Worker %d received %d\n", id, <-c)
-	}
+func createWorker(id int) chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			fmt.Printf("Worker %d received %d\n", id, <-c)
+		}
+	}()
+	return c
 }
 
 // DeadLockChanDemo
@@ -16,8 +20,7 @@ func worker(id int, c chan int) {
 func DeadLockChanDemo() {
 	var channels [10]chan int
 	for i := 0; i < 10; i++ {
-		channels[i] = make(chan int)
-		go worker(i, channels[i])
+		channels[i] = createWorker(i)
 	}
 	// 雖然依序傳入但Print的順序會不一樣，因為每個Print都是一個I/O操作，goroutine 會進行調度。
 	for i := 0; i < 10; i++ {
