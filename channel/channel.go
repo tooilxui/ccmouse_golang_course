b2a4c09e9352b561c8b06d97c229a0b43595d9d5
Saxon C <tooilxui@gmail.com>
2020-02-23 16:37:21 +0800

    channel: send chan as argument

--- channel/channel.go
+++ channel/channel.go
@@ -5,17 +5,17 @@ import (
 	"time"
 )
 
+func worker(id int, c chan int){
+	for {
+		fmt.Printf("Worker %d received %d\n",id, <-c)
+	}
+}
+
 // DeadLockChanDemo
 // channel 是拿來給 goroutine 間溝通的，必須透過 goroutine 收/放，否則會deadlock
 func DeadLockChanDemo() {
 	c := make(chan int)
-	go func() {
-		for {
-			// 收1印1、收2印2，但來不及印出2就結束程式了，先加一個sleep讓他順利印完
-			n:= <-c
-			fmt.Println(n)
-		}
-	}()
+	go worker(0,c)
 	c <- 1
 	c <- 2
 	time.Sleep(time.Millisecond)
