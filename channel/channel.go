8d5f0b595b498b3c99b399d3fe3779a049e848cd
Saxon C <tooilxui@gmail.com>
2020-02-23 16:44:06 +0800

    channel: chan slice

--- channel/channel.go
+++ channel/channel.go
@@ -5,21 +5,30 @@ import (
 	"time"
 )
 
-func worker(id int, c chan int){
+func worker(id int, c chan int) {
 	for {
-		fmt.Printf("Worker %d received %d\n",id, <-c)
+		fmt.Printf("Worker %d received %d\n", id, <-c)
 	}
 }
 
 // DeadLockChanDemo
 // channel 是拿來給 goroutine 間溝通的，必須透過 goroutine 收/放，否則會deadlock
 func DeadLockChanDemo() {
-	c := make(chan int)
-	go worker(0,c)
-	c <- 1
-	c <- 2
+	var channels [10]chan int
+	for i := 0; i < 10; i++ {
+		channels[i] = make(chan int)
+		go worker(i, channels[i])
+	}
+	// 雖然依序傳入但Print的順序會不一樣，因為每個Print都是一個I/O操作，goroutine 會進行調度。
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + 1
+	}
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'A' + 1
+	}
+
 	time.Sleep(time.Millisecond)
 }
-func main(){
+func main() {
 	DeadLockChanDemo()
 }
