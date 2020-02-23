ccf68cb00425ad6ae65431af9f9eafcaeebe1821
Saxon C <tooilxui@gmail.com>
2020-02-23 16:58:31 +0800

    channel: chan with buffer

--- channel/channel.go
+++ channel/channel.go
@@ -5,13 +5,14 @@ import (
 	"time"
 )
 
+func worker(id int, c chan int) {
+	for {
+		fmt.Printf("Worker %d received %d\n", id, <-c)
+	}
+}
 func createWorker(id int) chan<- int { // 加一個 <- 代表只能傳資料給chan
 	c := make(chan int)
-	go func() {
-		for {
-			fmt.Printf("Worker %d received %d\n", id, <-c)
-		}
-	}()
+	go worker(id, c)
 	return c
 }
 
@@ -32,6 +33,18 @@ func DeadLockChanDemo() {
 
 	time.Sleep(time.Millisecond)
 }
+
+// buffered channel : channel with size
+func bufferedChannel() {
+	c := make(chan int, 3)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd' // size=3 傳入4個? 只要取的速度夠快，保持不要超過size都可以運行
+	time.Sleep(time.Millisecond)
+}
 func main() {
-	DeadLockChanDemo()
+	//DeadLockChanDemo()
+	bufferedChannel()
 }
